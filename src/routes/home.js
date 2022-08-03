847e37666aa8db4ac658343f2ab86413b12918d1
ColeColombia <Benzemabenelzane@yahoo.com>
2022-08-03 12:33:55 +0200

    store subject and mark

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,8 +3,35 @@ import Select from 'react-select';
 
 import { languages, lifeOrientation, mathematics, other } from '../resources/subjects';
 import getMarks from '../resources/marks';
+import { useState } from 'react';
 
 export default function Home(){
+
+    const [apsData] = useState([]); 
+
+    function handleSubject(id, studentSubject){
+        
+        if(!apsData[id]){
+            apsData[id] = {};
+            apsData[id].subject = studentSubject;
+        }
+        else{
+            apsData[id].subject = studentSubject;
+        }
+
+    }
+
+    function handleMark(id, mark){
+
+        if(!apsData[id]){
+            apsData[id] = {};
+            apsData[id].mark = mark;
+        }
+        else{
+            apsData[id].mark = mark;
+        }
+
+    }
 
     return (
         <div className="container mt-5 pt-5">
@@ -13,11 +40,15 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={languages} 
-                    defaultValue={{value:"select first language", label:"select first language"}}/>
+                    defaultValue={{value:"select first language", label:"select first language"}}
+                    onChange={value => handleSubject(0, value.value)}
+                    />
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}/>
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}
+                onChange={value => handleMark(0, value.value)}
+                />
                 </div>
                     </div>
                 </div>
@@ -26,11 +57,13 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={languages} 
-                    defaultValue={{value:"select second language", label:"select second language"}}/>
+                    defaultValue={{value:"select second language", label:"select second language"}}
+                    onChange={value => handleSubject(1, value.value)}/>
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}/>
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}
+                onChange={value => handleMark(1, value.value)}/>
                 </div>
                     </div>
                 </div>
@@ -39,11 +72,13 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={mathematics} 
-                    defaultValue={{value:"select mathematics", label:"select mathematics"}} />
+                    defaultValue={{value:"select mathematics", label:"select mathematics"}} 
+                    onChange={value => handleSubject(2, value.value)}/>
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}/>
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}
+                onChange={value => handleMark(2, value.value)}/>
                 </div>
                     </div>
                 </div>
@@ -52,11 +87,13 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={lifeOrientation} 
-                    defaultValue={{value:"select Life orientation", label:"select Life orientation"}} />
+                    defaultValue={{value:"select Life orientation", label:"select Life orientation"}} 
+                    onChange={value => handleSubject(3, value.value)}/>
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}/>
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}
+                onChange={value => handleMark(3, value.value)}/>
                 </div>
                     </div>
                 </div>
@@ -65,11 +102,13 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={other} 
-                    defaultValue={{value:"select 5th subject", label:"select 5th subject"}} />
+                    defaultValue={{value:"select 5th subject", label:"select 5th subject"}} 
+                    onChange={value => handleSubject(4, value.value)}/>
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}} />
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}} 
+                onChange={value => handleMark(4, value.value)}/>
                 </div>
                     </div>
                 </div>
@@ -78,11 +117,13 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={other} 
-                    defaultValue={{value:"select 6th subject", label:"select 6th subject"}} />
+                    defaultValue={{value:"select 6th subject", label:"select 6th subject"}} 
+                    onChange={value => handleSubject(5, value.value)}/>
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}} />
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}}
+                onChange={value => handleMark(5, value.value)} />
                 </div>
                     </div>
                 </div>
@@ -91,11 +132,13 @@ export default function Home(){
                     <div className="row g-2 subject-block">
                     <div className="col-lg-6">
                     <Select options={other} 
-                    defaultValue={{value:"select 7th subject", label:"select 7th subject"}} />
+                    defaultValue={{value:"select 7th subject", label:"select 7th subject"}} 
+                    onChange={value => handleSubject(6, value.value)}/>
                 </div>
                 <div className="col-lg-6">
                 <Select options={getMarks(100)} 
-                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}} />
+                defaultValue={{value:"select your mark (%)", label:"select your mark (%)"}} 
+                onChange={value => handleMark(6, value.value)}/>
                 </div>
                     </div>
                 </div>
